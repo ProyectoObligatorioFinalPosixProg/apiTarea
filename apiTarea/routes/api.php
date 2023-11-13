b52a1dda0d5a0990f966dd1e73b67f337a5fa275
Mavaa13 <mvesuperior@gmail.com>
2023-11-13 15:19:14 -0300

    CRUD y Middleware hecho, falta extraer el ID del usuario

--- apiTarea/routes/api.php
+++ apiTarea/routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TareaController;
+use App\Http\middleware\Autenticacion;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,8 +19,8 @@ use App\Http\Controllers\TareaController;
 Route::prefix('v1')->group(function () {
     Route::get("/tarea",[TareaController::class,'ListarTarea']);
     Route::get("/tarea/{id}",[TareaController::class,'BuscarTarea']);
-    Route::post("/tarea",[TareaController::class,'CrearTarea']);
-    Route::delete("/tarea/{id}",[TareaController::class,'EliminarTarea']);
-    Route::put("/tarea/{id}",[TareaController::class,'ModificarTarea']);
+    Route::post("/tarea",[TareaController::class,'CrearTarea']) -> middleware(Autenticacion::class);
+    Route::delete("/tarea/{id}",[TareaController::class,'EliminarTarea']) -> middleware(Autenticacion::class);
+    Route::put("/tarea/{id}",[TareaController::class,'ModificarTarea']) -> middleware(Autenticacion::class);
 
 });
